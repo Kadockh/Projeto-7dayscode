d3bff5f30fff4b4a5442cd2a6701fa080351b94a
Andrey Kadoch <159218130+Kadockh@users.noreply.github.com>
2025-01-14 13:08:07 -0300

    Add files via upload

---
+++ assets/javascript/Javascript.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".header__hamburger");
+    const nav = document.querySelector(".header__nav");
+  
+    hamburger.addEventListener("click", () => {
+      nav.classList.toggle("open");
+    });
+  });
