d5b749dde27953a589a46efe12a4330cb1814f77
Andrey Kadoch <159218130+Kadockh@users.noreply.github.com>
2025-01-21 17:34:14 -0300

    Add files via upload

---
+++ 7dayscode/js/Javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".header__hamburger");
+    const nav = document.querySelector(".header__nav");
+  
+    hamburger.addEventListener("click", () => {
+      nav.classList.toggle("open");
+    });
+  });
+
